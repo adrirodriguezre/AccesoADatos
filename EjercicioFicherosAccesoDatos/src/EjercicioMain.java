b2312a393d5c2624c8ea079028638ca95982a5c2
adrirodriguezre <59870702+adrirodriguezre@users.noreply.github.com>
2020-11-06 14:33:36 +0100

    Add files via upload
    0

--- EjercicioFicherosAccesoDatos/src/EjercicioMain.java
+++ EjercicioFicherosAccesoDatos/src/EjercicioMain.java
@@ -7,15 +7,14 @@ public class EjercicioMain {
 		
 		ProcesamientoFicheroPlano p1= new ProcesamientoFicheroPlano();
 		
-		//String ruta= "C:\\Users\\PC33\\Desktop\\Personajes.txt";
-		String home = System.getProperty("user.home");
-		String ruta=(home + File.separator + "Desktop" + File.separator + "libros.txt");
+		String ruta= "C:\\Users\\PC33\\Desktop\\libros.txt";
+		String rutaXML= "C:\\Users\\PC33\\Desktop\\libros.xml";
+	
 	
 		
 		ArrayList<Libro> Prueba = new ArrayList();
 		
 		Prueba=p1.leerFichero(ruta);
-		//System.out.println(Prueba);
 
 	}
 
